0bc86c513e22cfb9bbd3776cd8863078ae3cd2ec
Dev Patel <devp61136@gmail.com>
2021-11-18 18:02:57 +0530

    Attempt 2

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 console.log('Booting up the server! Please wait until finished...')
 server.listen(process.env.PORT || 3000, function(){
   var addr = server.address();
-  console.log("All ready! Server listening at", addr.address + ":" + addr.port);
+  console.log("All ready! Server listening at", addr.port);
 });
